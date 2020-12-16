bef7f84822b3d73d719e4557a76557d4884b1bb5
Carlos Martín <karuvsentor@gmail.com>
2020-12-16 20:26:18 +0100

    fixed background movement

--- javascript/background.js
+++ javascript/background.js
@@ -8,8 +8,7 @@ class Background {
         this.width = this.ctx.canvas.width
 
         this.img = new Image()
-        this.img.src = '../scripts/backgrounds/fondo 5.png'
-
+        this.img.src = './scripts/backgrounds/fondo-5.png'
         this.img.isLoad = false
         this.img.onload = () => {
             this.img.isLoad = true
@@ -34,17 +33,16 @@ class Background {
             this.ctx.drawImage(
                 this.img,
                 0,
-                this.y + this.height,
+                this.y - this.height,
                 this.width,
-                this.height
+                this.height,
             )
         }
     }
 
     move() {
         this.y += this.vy
-        console.log(this.y)
-        if (this.y + this.height >= this.ctx.canvas.height) {
+        if (this.y >= this.ctx.canvas.height) {
             this.y = 0
         }
 
